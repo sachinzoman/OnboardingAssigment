0881e7cd904a26ba1ddceda760367b0bd7870570
Sachin <sachin.b@zomato.com>
2019-07-01 17:59:11 +0530

    files deleted

--- restaurant_proto_gen.sh
+++
@@ -1,5 +0,0 @@
-# protoc --proto_path=./../../protos --go_out=./ --grpc_out=./  --plugin=protoc-gen-grpc=grpc_go_plugin ./../../protos/restaurant.proto
-#  protoc -I restaurant/ restaurant/restaurant.proto --go_out=plugins=grpc:restaurant
-protoc --proto_path=./restaurant --go_out=plugins=grpc:restaurant restaurant.proto
-# protoc -I restaurant --go_out=/restaurant /restaurant/restaurant.proto
-# protoc --proto_path=./restaurant --go_out=./restaurant --plugin=grpc_go_plugin restaurant.proto
